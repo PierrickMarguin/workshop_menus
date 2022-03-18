ac3646218da768e6d05632f4c8a7c222f09ce975
pierrick marguin <pierrick.marguin@epitech.eu>
2022-03-18 13:52:23 +0100

    RESONANCES DE SCHUMANN

--- workshop/include/menus/menu_1.h
+++ workshop/include/menus/menu_1.h
@@ -8,6 +8,36 @@
 #ifndef MENU_1_H_
 #define MENU_1_H_
 
+#include "workshop/include/menu.h"
 
+static event_function events[sfEvtCount][MAX_EVENT_COUNT] = {
+    {event_closed, NULL}, // sfEvtClosed
+    {event_resized, NULL}, // sfEvtResized
+    {NULL}, // sfEvtLostFocus
+    {NULL}, // sfEvtGainedFocus
+    {NULL}, // sfEvtTextEntered
+    {NULL}, // sfEvtKeyPressed
+    {move_to_menu, NULL}, // sfEvtKeyReleased
+    {NULL}, // sfEvtMouseWheelMoved
+    {NULL}, // sfEvtMouseWheelScrolled
+    {event_mouse_button_pressed, NULL}, // sfEvtMouseButtonPressed
+    {event_mouse_button_released, NULL}, // sfEvtMouseButtonReleased
+    {event_mouse_moved, NULL}, // sfEvtMouseMoved
+    {NULL}, // sfEvtMouseEntered
+    {NULL}, // sfEvtMouseLeft
+    {NULL}, // sfEvtJoystickButtonPressed
+    {NULL}, // sfEvtJoystickButtonReleased
+    {NULL}, // sfEvtJoystickMoved
+    {NULL}, // sfEvtJoystickConnected
+    {NULL}, // sfEvtJoystickDisconnected
+    {NULL}, // sfEvtTouchBegan
+    {NULL}, // sfEvtTouchMoved
+    {NULL}, // sfEvtTouchEnded
+    {NULL} // sfEvtSensorChanged
+};
+
+int draw_menu_background(game_t *game, menu_t *menu);
+
+static draw_function draws[] = {draw_menu_background, NULL};
 
 #endif /* MENU_1_H_ */
--- workshop/src/create/menu/create_menu_4.c
+++ workshop/src/create/menu/create_menu_4.c
@@ -10,39 +10,10 @@
 #include "event.h"
 
 #include "workshop/include/menu.h"
+#include "workshop/include/menus/menu_4.h"
 
 int copy_draw_tab(menu_t *menu, draw_function *tab);
 int copy_event_tab(menu_t *menu, event_function tab[sfEvtCount][MAX_EVENT_COUNT]);
-
-int draw_menu_background(game_t *game, menu_t *menu);
-
-static event_function events[sfEvtCount][MAX_EVENT_COUNT] = {
-    {event_closed, NULL}, // sfEvtClosed
-    {event_resized, NULL}, // sfEvtResized
-    {NULL}, // sfEvtLostFocus
-    {NULL}, // sfEvtGainedFocus
-    {NULL}, // sfEvtTextEntered
-    {NULL}, // sfEvtKeyPressed
-    {move_to_menu, NULL}, // sfEvtKeyReleased
-    {NULL}, // sfEvtMouseWheelMoved
-    {NULL}, // sfEvtMouseWheelScrolled
-    {event_mouse_button_pressed, NULL}, // sfEvtMouseButtonPressed
-    {event_mouse_button_released, NULL}, // sfEvtMouseButtonReleased
-    {event_mouse_moved, NULL}, // sfEvtMouseMoved
-    {NULL}, // sfEvtMouseEntered
-    {NULL}, // sfEvtMouseLeft
-    {NULL}, // sfEvtJoystickButtonPressed
-    {NULL}, // sfEvtJoystickButtonReleased
-    {NULL}, // sfEvtJoystickMoved
-    {NULL}, // sfEvtJoystickConnected
-    {NULL}, // sfEvtJoystickDisconnected
-    {NULL}, // sfEvtTouchBegan
-    {NULL}, // sfEvtTouchMoved
-    {NULL}, // sfEvtTouchEnded
-    {NULL} // sfEvtSensorChanged
-};
-
-static draw_function draws[] = {draw_menu_background, NULL};
 
 int create_menu_4(game_t *game, menu_t *menu)
 {
